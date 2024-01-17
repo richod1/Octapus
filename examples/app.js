c05cdb6407f632d14140813a9979e1d4a70c7bc5
richod1 <frimpsup@gmail.com>
2024-01-17 13:44:43 +0000

    octapus auto chages render added

--- examples/app.js
+++ examples/app.js
@@ -2,12 +2,8 @@ const {Router,createServer} =require("../lib/octapus")
 
 const router=new Router();
 
-router.get("/",(req,res)=>{
-    res.write(200,{"Content-Type":"text/plain"})
-    res.send("Hello Octapus")
-})
+router.get("/",(req,res)=>res.end("Hello Octapus"))
 
-createServer(router).listen(3000,(err)=>{
-    if(err) throw new Error("server failed")
+createServer(router).listen(3000,()=>{
     console.log(`server is up on port 3000`)
 })
