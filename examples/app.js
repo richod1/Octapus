8512ab3c4edeb8a3c3dba80f97c7df9448979fb0
richod1 <frimpsup@gmail.com>
2024-01-18 08:06:50 +0000

    static file serve added

--- examples/app.js
+++ examples/app.js
@@ -4,6 +4,29 @@ const router=new Router();
 
 router.get("/",(req,res)=>res.end("Hello Octapus"))
 
+
+const base_route=router.get("/app",(req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    res.end("Hello Octapus")
+})
+router.merge(base_route)
+
+function getUserList(req,res){
+    res.writeHead(200,{"Content-Type":"application/json"})
+    res.end("/users: " + JSON.stringify({user:["degraft","Frimpong"]}))
+}
+
+router.get("/users",getUserList)
+
+router.get("/add",(req,res)=>{
+    const htmlContent=`
+    <html>
+    <h1>Hello Octapus</h1>
+    </html>
+    `
+    res.sendHtmlResponse(htmlContent)
+})
+
 createServer(router).listen(3000,()=>{
     console.log(`server is up on port 3000`)
 })
