24a33a6ec14d02d7d40b8cd1ab4f6aa767a2a34e
richod1 <frimpsup@gmail.com>
2024-01-23 23:35:44 +0000

    auth-middleware added

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports=require("./lib/octapus")
 // ORM database handler
 module.exports=require("./lib/octapus-orm")
+
+// auth middleware
+module.exports=require("./lib/middleware/authMiddlware")
